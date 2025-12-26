ad3ef4ae30b60e6ffffd26e4a2291dca0342fdbb
Juvin <hdcqW@outlook.com>
2025-12-26 16:27:26 +0800

    调整User

--- src/Model/User.cpp
+++ src/Model/User.cpp
@@ -2,14 +2,12 @@
 
 #include<QString>
 
-User::User(QString id, QString password, QString name, int role, double credit, int avatar_id, bool is_active):
-    id(id), password(password), name(name), role(role), credit(credit), avatar_id(avatar_id), is_active(is_active){
-}
+User::User(QString id, QString name, QString password, int role, double credit, bool is_active):
+    id(id), name(name), password(password), role(role), credit(credit), is_active(is_active){}
 
 const QString& User::get_id() const{ return id; }
-const QString& User::get_password() const{ return password; }   
 const QString& User::get_name() const{ return name; }
+const QString& User::get_password() const{ return password; }   
 int User::get_role() const{ return role; }
 double User::get_credit() const{ return credit; }
-int User::get_avatar_id() const{ return avatar_id; }
 bool User::get_is_active() const{ return is_active; }
--- src/Model/User.h
+++ src/Model/User.h
@@ -9,22 +9,20 @@
 
 class User {
 public:
-    User(QString id, QString password, QString name, int role, double credit, int avatar_id, bool is_active);
+    User(QString id, QString name, QString password, int role, double credit, bool is_active);
     ~User() = default;
     //getters
     const QString& get_id() const;
-    const QString& get_password() const;
     const QString& get_name() const;
+    const QString& get_password() const;
     int get_role() const;
     double get_credit() const;
-    int get_avatar_id() const;
     bool get_is_active() const;
 private:
     QString id;
-    QString password;
     QString name;
+    QString password;
     int role;
     double credit;  //一卡通余额
-    int avatar_id; //头像资源索引
     bool is_active; //是否已激活，0:未激活需首次设置密码, 1:已激活
 };
